1c127eae80f3b9c51963fa488805257f18682bf2
ArthurDias25 <arthurgameplays2@gmail.com>
2020-08-17 11:12:52 -0300

    Alterações na Pagina de Rank

--- app/Http/Controllers/Admin/GameController.php
+++ app/Http/Controllers/Admin/GameController.php
@@ -11,14 +11,42 @@ class GameController extends Controller
 {
     public function rank(){
         
+        $orders = DB::table('listings')
+        ->join('games','games.id','=','listings.Id_Game')
+        ->select('listings.Nota','listings.Id_Game','games.id')
+        ->get();
+
+
         $games = DB::table('games')
         ->join('game_categories', 'games.Id_GameCategories', '=', 'game_categories.id')
         ->select('games.*','game_categories.Nome_GameCategory')
         ->get();
 
 
+        $genres = DB::table('game_genres')
+        ->join('games','game_genres.Id_Game','=','games.id')
+        ->join('genres','genres.id','=','game_genres.Id_Genero')
+        ->select('game_genres.*','games.id','genres.genero')
+        ->get();
+        
+        $developers = DB::table('game_developers')
+        ->join('games','games.id','=','game_developers.Id_Game')
+        ->join('developers','developers.id','=','game_developers.Id_Developer')
+        ->select('game_developers.*','developers.Desenvolvedora','games.id')
+        ->get();
+
+        $platforms = DB::table('game_ports')
+        ->join('games','games.id','=','game_ports.Id_Game')
+        ->join('platforms','platforms.id','=','game_ports.Id_Plataforma')
+        ->select('game_ports.*','games.id','platforms.Plataforma')
+        ->get();
+        
         return view('public.rank', [
         'games' => $games,
+        'developers' => $developers,
+        'genres' => $genres,
+        'platforms' => $platforms,
+        'orders' => $orders
     ]);
     }
 }
--- resources/views/public/rank.blade.php
+++ resources/views/public/rank.blade.php
@@ -9,6 +9,9 @@
             <td> Jogo </td>
             <td> Categoria </td>
             <td> Genero </td>
+            <td> Desenvolvedora </td>
+            <td> Plataforma </td>
+            <td> Media</td>
         </tr>
     </thead>
     <tbody>
@@ -20,6 +23,34 @@
                 </td>
                 <td> {{$game->Nome_Jogo}}</td>
                 <td>{{ $game->Nome_GameCategory}}</td>
+                <td>
+                    @foreach ($genres as $genre)
+                        @if ($genre->Id_Game == $game->id)
+                            <p> {{$genre->genero}} </p>
+                        @endif
+                    @endforeach    
+                </td>
+                <td>
+                    @foreach ($developers as $developer)
+                        @if ($developer->Id_Game == $game->id)
+                           <p> {{$developer->Desenvolvedora}} </p>
+                        @endif
+                    @endforeach
+                </td>
+                <td>
+                    @foreach ($platforms as $platform)
+                        @if ($platform->Id_Game == $game->id)
+                           <p> {{$platform->Plataforma}} </p>
+                        @endif
+                    @endforeach
+                </td>
+                <td>
+                    @foreach ($orders as $order)
+                        @if ($order->Id_Game == $game->id)
+                            <p> {{$order->Nota}}</p>
+                        @endif
+                    @endforeach
+                </td>
             </tr>
         @endforeach
 
